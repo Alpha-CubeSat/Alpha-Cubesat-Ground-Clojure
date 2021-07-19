768725c5d7d6fabc36fb1aa1531a41b6be5370c7
Eric Zhang <your_netid@cornell.edu>
2021-07-19 08:59:51 -0500

    asdf

--- cubesat-backend/target/classes/cubesat_clj/handler/main.clj
+++
@@ -1,1 +0,0 @@
-(do (clojure.core/ns cubesat-clj.handler.main (:gen-class)) (clojure.core/defn -main [] ((do (clojure.core/require (quote ring.server.leiningen)) (clojure.core/resolve (quote ring.server.leiningen/serve))) (quote {:ring {:handler cubesat-clj.handler/app, :open-browser? false, :stacktraces? false, :auto-reload? false, :auto-refresh? false}}))))
